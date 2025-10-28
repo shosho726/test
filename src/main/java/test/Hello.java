1480018871fe006040524fa553091925b032e377
Shota Takeuchi <shota.ham.0523@icloud.com>
2025-10-28 18:07:33 +0900

    first commit

---
+++ src/main/java/test/Hello.java
@@ -0,0 +1,22 @@
+package test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns={"/chapter3/hello"})
+public class Hello extends HttpServlet {
+	
+	public void doGet (
+		HttpServletRequest req, HttpServletResponse resp
+	) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("Hello Servlet!");
+		out.println(new java.util.Date());
+	}
+}
